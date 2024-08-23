e943764158d386c0634318db9aa04415d61e7e04
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-24 02:24:02 +0600

    SSL VR Stubs updated

---
+++ database/seeders/WalletToWalletSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fintech\Reload\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WalletToWalletSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
