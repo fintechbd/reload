5daeb0134054a1432ad724f180a916eafff36313
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-09 01:41:38 +0600

    dubai bank seeder added

--- database/seeders/Bangladesh/BankDepositSeeder.php
+++ database/seeders/Bangladesh/BankDepositSeeder.php
@@ -29,7 +29,6 @@ class BankDepositSeeder extends Seeder
             }
         }
     }
-
     private function data(): array
     {
         $image_svg = base_path('vendor/fintech/reload/resources/img/service_type/logo_svg/');
