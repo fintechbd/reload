b0f09d05a290b74d21452c4cd595caf7fc05cdf6
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-11 15:36:18 +0600

    ca ans ae wallet image added

--- database/seeders/Canada/PaymentGatewayDepositSeeder.php
+++ database/seeders/Canada/PaymentGatewayDepositSeeder.php
@@ -18,19 +18,9 @@ class PaymentGatewayDepositSeeder extends Seeder
 
             $fundDepositParent = Business::serviceType()->list(['service_type_slug' => 'fund_deposit'])->first();
 
-            $entries = $this->data();
-
             $servingCountries = MetaData::country()->servingIds(['iso2' => 'CA']);
 
-            Business::serviceTypeManager($entries[0], $fundDepositParent)
-                ->srcCountries($servingCountries)
-                ->enabled()
-                ->execute();
-
-            $interactParent = Business::serviceType()->list(['service_type_slug' => 'interac_e_transfer'])->first();
-
-            Business::serviceTypeManager($entries[1], $interactParent)
-                ->hasService()
+            Business::serviceTypeManager($this->data(), $fundDepositParent)
                 ->srcCountries($servingCountries)
                 ->enabled()
                 ->execute();
@@ -43,29 +33,19 @@ class PaymentGatewayDepositSeeder extends Seeder
         $image_png = base_path('vendor/fintech/reload/resources/img/service_type/logo_png/');
 
         return [
-            [
-                'service_type_name' => 'INTERAC E TRANSFER',
-                'service_type_slug' => 'interac_e_transfer',
-                'logo_svg' => "{$image_svg}interac_e_transfer.svg",
-                'logo_png' => "{$image_png}interac_e_transfer.png",
-                'service_type_is_parent' => 'yes',
-                'service_type_is_description' => 'no',
-            ],
-            //child of inter e transfer
-            [
-                'service_type_name' => 'CIBC Bank',
-                'service_type_slug' => 'cibc_bank',
-                'logo_svg' => "{$image_svg}cibc_bank.svg",
-                'logo_png' => "{$image_png}cibc_bank.png",
-                'service_type_is_parent' => 'no',
-                'service_type_is_description' => 'no',
-                'service_stat_data' => [
-                    'local_currency_lower_limit' => '1000',
-                    'local_currency_higher_limit' => '2500',
-                    'charge' => '1%',
-                    'discount' => '2%',
-                    'commission' => '0',
-                ],
+            'service_type_name' => 'INTERAC E TRANSFER',
+            'service_type_slug' => 'interac_e_transfer',
+            'logo_svg' => "{$image_svg}interac_e_transfer.svg",
+            'logo_png' => "{$image_png}interac_e_transfer.png",
+            'service_type_is_parent' => 'no',
+            'service_type_is_description' => 'no',
+            'service_stat_data' => [
+                'local_currency_lower_limit' => '1000',
+                'local_currency_higher_limit' => '2500',
+                'transactional_currency' => 'CAD',
+                'charge' => '1%',
+                'discount' => '2%',
+                'commission' => '0',
             ],
         ];
     }
