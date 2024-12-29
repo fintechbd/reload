7d72cabc4439e79efa61440d1716a7b1a0476af3
hafijul233 <hafijul233@users.noreply.github.com>
2024-12-29 12:56:33 +0000

    Fix styling

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -47,7 +47,7 @@ class InstallCommand extends Command
                     'logo_png' => $this->image_png.'fund_deposit.png',
                     'service_type_is_parent' => 'yes',
                     'service_type_is_description' => 'no',
-                    'service_type_step' => '1'
+                    'service_type_step' => '1',
                 ],
                 [
                     'service_type_name' => 'Withdraw',
@@ -56,7 +56,7 @@ class InstallCommand extends Command
                     'logo_png' => $this->image_png.'withdraw.png',
                     'service_type_is_parent' => 'yes',
                     'service_type_is_description' => 'no',
-                    'service_type_step' => '1'
+                    'service_type_step' => '1',
                 ],
             ];
             foreach ($serviceTypes as $entry) {
