d223475265bd0bf4d151f95f79d77b672e999a9b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-09 20:58:14 +0600

    payout event testing issue fixed

--- src/Events/DepositRejected.php
+++ src/Events/DepositRejected.php
@@ -4,9 +4,7 @@ namespace Fintech\Reload\Events;
 
 use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Reload\Models\Deposit;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -26,15 +24,4 @@ class DepositRejected
         $this->deposit = $deposit;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
 }
