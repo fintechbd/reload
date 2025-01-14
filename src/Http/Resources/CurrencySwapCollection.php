893a27b8946475d069756520e530e6a0a569f5d5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-15 02:27:00 +0600

    currency swap field added

--- src/Http/Resources/CurrencySwapCollection.php
+++ src/Http/Resources/CurrencySwapCollection.php
@@ -21,24 +21,20 @@ class CurrencySwapCollection extends ResourceCollection
         return $this->collection->map(function ($currencySwap) {
             $data = [
                 'id' => $currencySwap->getKey(),
-                'source_country_id' => $currencySwap->source_country_id ?? null,
+//                 'source_country_id' => $currencySwap->source_country_id ?? null,
                 'source_country_name' => null,
-                'destination_country_id' => $currencySwap->destination_country_id ?? null,
+//                 'destination_country_id' => $currencySwap->destination_country_id ?? null,
                 'destination_country_name' => null,
-                'parent_id' => $currencySwap->parent_id ?? null,
-                'sender_receiver_id' => $currencySwap->sender_receiver_id ?? null,
+//                 'parent_id' => $currencySwap->parent_id ?? null,
+//                 'sender_receiver_id' => $currencySwap->sender_receiver_id ?? null,
                 'sender_receiver_name' => null,
-                'user_id' => $currencySwap->user_id ?? null,
+//                 'user_id' => $currencySwap->user_id ?? null,
                 'user_name' => null,
-                'service_id' => $currencySwap->service_id ?? null,
+//                 'service_id' => $currencySwap->service_id ?? null,
                 'service_name' => null,
-                'transaction_form_id' => $currencySwap->transaction_form_id ?? null,
+//                 'transaction_form_id' => $currencySwap->transaction_form_id ?? null,
                 'transaction_form_name' => $currencySwap->transaction_form_name ?? null,
                 'ordered_at' => $currencySwap->ordered_at ?? null,
-                'amount' => $currencySwap->amount ?? null,
-                'currency' => $currencySwap->currency ?? null,
-                'converted_amount' => $currencySwap->converted_amount ?? null,
-                'converted_currency' => $currencySwap->converted_currency ?? null,
                 'order_number' => $currencySwap->order_number ?? null,
                 'risk_profile' => $currencySwap->risk_profile ?? null,
                 'notes' => $currencySwap->notes ?? null,
@@ -47,7 +43,7 @@ class CurrencySwapCollection extends ResourceCollection
                 'status' => $currencySwap->status ?? null,
                 'created_at' => $currencySwap->created_at ?? null,
                 'updated_at' => $currencySwap->updated_at ?? null,
-            ];
+            ] + $currencySwap->commonAttributes();
 
             if (Core::packageExists('MetaData')) {
                 $data['source_country_name'] = $currencySwap->sourceCountry?->name ?? null;
--- src/Http/Resources/CurrencySwapResource.php
+++ src/Http/Resources/CurrencySwapResource.php
@@ -2,8 +2,10 @@
 
 namespace Fintech\Reload\Http\Resources;
 
+use Fintech\Core\Facades\Core;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use stdClass;
 
 class CurrencySwapResource extends JsonResource
 {
@@ -15,6 +17,50 @@ class CurrencySwapResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data = [
+                'id' => $this->getKey(),
+//                 'source_country_id' => $this->source_country_id ?? null,
+                'source_country_name' => null,
+//                 'destination_country_id' => $this->destination_country_id ?? null,
+                'destination_country_name' => null,
+//                 'parent_id' => $this->parent_id ?? null,
+//                 'sender_receiver_id' => $this->sender_receiver_id ?? null,
+                'sender_receiver_name' => null,
+//                 'user_id' => $this->user_id ?? null,
+                'user_name' => null,
+//                 'service_id' => $this->service_id ?? null,
+                'service_name' => null,
+//                 'transaction_form_id' => $this->transaction_form_id ?? null,
+                'transaction_form_name' => $this->transaction_form_name ?? null,
+                'ordered_at' => $this->ordered_at ?? null,
+                'order_number' => $this->order_number ?? null,
+                'risk_profile' => $this->risk_profile ?? null,
+                'notes' => $this->notes ?? null,
+                'is_refunded' => $this->is_refunded ?? null,
+                'order_data' => $this->order_data ?? new stdClass,
+                'status' => $this->status ?? null,
+                'created_at' => $this->created_at ?? null,
+                'updated_at' => $this->updated_at ?? null,
+            ] + $this->commonAttributes();
+
+        if (Core::packageExists('MetaData')) {
+            $data['source_country_name'] = $this->sourceCountry?->name ?? null;
+            $data['destination_country_name'] = $this->destinationCountry?->name ?? null;
+        }
+        if (Core::packageExists('Auth')) {
+            $data['user_name'] = $this->user?->name ?? null;
+            $data['sender_receiver_name'] = $this->senderReceiver?->name ?? null;
+        }
+        if (Core::packageExists('Business')) {
+            $data['service_name'] = $this->service?->service_name ?? null;
+        }
+        if (Core::packageExists('Business')) {
+            $data['service_name'] = $this->service?->service_name ?? null;
+        }
+        if (Core::packageExists('Transaction')) {
+            $data['transaction_form_name'] = $this->transactionForm?->name ?? null;
+        }
+
+        return $data;
     }
 }
