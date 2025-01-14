472681bdf921956aebb36836a0c9d6e4bb7425e5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-15 02:30:35 +0600

    request money field added

--- src/Http/Resources/RequestMoneyCollection.php
+++ src/Http/Resources/RequestMoneyCollection.php
@@ -21,24 +21,20 @@ class RequestMoneyCollection extends ResourceCollection
         return $this->collection->map(function ($requestMoney) {
             $data = [
                 'id' => $requestMoney->getKey(),
-                'source_country_id' => $requestMoney->source_country_id ?? null,
+//                 'source_country_id' => $requestMoney->source_country_id ?? null,
                 'source_country_name' => null,
-                'destination_country_id' => $requestMoney->destination_country_id ?? null,
+//                 'destination_country_id' => $requestMoney->destination_country_id ?? null,
                 'destination_country_name' => null,
-                'parent_id' => $requestMoney->parent_id ?? null,
-                'sender_receiver_id' => $requestMoney->sender_receiver_id ?? null,
+//                 'parent_id' => $requestMoney->parent_id ?? null,
+//                 'sender_receiver_id' => $requestMoney->sender_receiver_id ?? null,
                 'sender_receiver_name' => null,
-                'user_id' => $requestMoney->user_id ?? null,
+//                 'user_id' => $requestMoney->user_id ?? null,
                 'user_name' => null,
-                'service_id' => $requestMoney->service_id ?? null,
+//                 'service_id' => $requestMoney->service_id ?? null,
                 'service_name' => null,
-                'transaction_form_id' => $requestMoney->transaction_form_id ?? null,
+//                 'transaction_form_id' => $requestMoney->transaction_form_id ?? null,
                 'transaction_form_name' => $requestMoney->transaction_form_name ?? null,
                 'ordered_at' => $requestMoney->ordered_at ?? null,
-                'amount' => $requestMoney->amount ?? null,
-                'currency' => $requestMoney->currency ?? null,
-                'converted_amount' => $requestMoney->converted_amount ?? null,
-                'converted_currency' => $requestMoney->converted_currency ?? null,
                 'order_number' => $requestMoney->order_number ?? null,
                 'risk_profile' => $requestMoney->risk_profile ?? null,
                 'notes' => $requestMoney->notes ?? null,
@@ -47,7 +43,7 @@ class RequestMoneyCollection extends ResourceCollection
                 'status' => $requestMoney->status ?? null,
                 'created_at' => $requestMoney->created_at ?? null,
                 'updated_at' => $requestMoney->updated_at ?? null,
-            ];
+            ] + $requestMoney->commonAttributes();
 
             if (Core::packageExists('MetaData')) {
                 $data['source_country_name'] = $requestMoney->sourceCountry?->name ?? null;
