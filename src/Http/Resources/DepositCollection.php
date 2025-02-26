88e0c28adec1dd3b8570fa403935b5ac5ef71ee2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-02-26 17:59:51 +0600

    deposit testing

--- src/Http/Resources/DepositCollection.php
+++ src/Http/Resources/DepositCollection.php
@@ -18,7 +18,7 @@ class DepositCollection extends ResourceCollection
     {
         return $this->collection->map(function ($item) {
             return [
-                'parent_order_number' => $item->parent_order?->order_number ?? null,
+                'parent_order_number' => $item->parent_order ?? null,
                 'slip' => $item->getFirstMediaUrl('slip') ?? null,
                 'risk' => $item->risk ?? null,
                 'is_refunded' => $item->is_refunded ?? null,
